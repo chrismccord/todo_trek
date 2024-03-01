94bbf4467a53cbeb4b3c493225bf31e49f80ce01
Jason Stiebs <jason@fly.io>
2024-03-01 15:02:06 -0600

    Fix trying to drag a new item

--- assets/js/app.js
+++ assets/js/app.js
@@ -40,12 +40,14 @@ Hooks.Sortable = {
     let group = this.el.dataset.group
     let isDragging = false
     this.el.addEventListener("focusout", e => isDragging && e.stopImmediatePropagation())
+
     let sorter = new Sortable(this.el, {
       group: group ? {name: group, pull: true, put: true} : undefined,
       animation: 150,
       dragClass: "drag-item",
       ghostClass: "drag-ghost",
-      onStart: e => isDragging = true, // prevent phx-blur from firing while dragging
+      filter: ".no-drag",
+      onStart: e =>  isDragging = true, // prevent phx-blur from firing while dragging
       onEnd: e => {
         isDragging = false
         let params = {old: e.oldIndex, new: e.newIndex, to: e.to.dataset, ...e.item.dataset}
